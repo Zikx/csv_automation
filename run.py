97542ed1651d6342eb3965cb63f9ff7566d25875
zikx <8687706@gmail.com>
2021-04-16 17:27:20 +0900

    ADD manufacture_id and fixsturesystemId

--- run.py
+++ run.py
@@ -1,15 +1,29 @@
 import os
 import os.path
 import csv
+import pandas as pd
 
 # input root_dir
 root_dir = input('root_dir : ')
-from_mongo_id = root_dir + '_id'
+
+# csv_system_manu_id
+csv_system_manu_id = pd.read_csv(f'{root_dir}_id.csv',
+    names = ['systemId', 'manuId', 'systemName']
+)
 
 # create csv and row
 f = open('write.csv','w', newline='')
 wr = csv.writer(f)
 wr.writerow(["_manufacturerId", "occlusalDiameter", "apicalDiameter", "length", "stlFilePath", "_defaultAbutmentId", "_fixturesystemId", "fixtureName", "gingivaHeight"])
+
+def search_id(val):
+    row_idx = int(csv_system_manu_id.index[csv_system_manu_id["systemName"] == val].tolist()[0])
+
+    system_id = csv_system_manu_id.iloc[row_idx][0]
+    manufact_id = csv_system_manu_id.iloc[row_idx][1]    
+
+    return system_id, manufact_id
+
 
 # export dir  
 for dir_path, dirs, files in os.walk(root_dir):
@@ -18,10 +32,13 @@ for dir_path, dirs, files in os.walk(root_dir):
     for file_name in files:
         file_code = file_name[:-3]
         file_path = dir_path + '\\' + file_code
-        wr.writerow([root_dir, '','','',file_path, '',system_name, file_code, 0.0])
+        
+        system_id, manufact_id = search_id(system_name)
+
+        wr.writerow([manufact_id, '','','',file_path, '',system_id, file_code, 0.0])
 
         print(f'## file : {file_path} ')
 
-
-
 f.close()
+
+
