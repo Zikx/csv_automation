11be987ce017fd563626485a48e3c4c98387a83a
zikx <8687706@gmail.com>
2021-04-16 18:16:16 +0900

    Bug : csv_OAL에서 일부가 긁히지 못함. (TP3105L, SYR3507) 참고 / system이 중복되어 발생하는 문제일수도 있음

--- run.py
+++ run.py
@@ -11,6 +11,11 @@ csv_system_manu_id = pd.read_csv(f'{root_dir}_id.csv',
     names = ['systemId', 'manuId', 'systemName']
 )
 
+# csv_OAL
+csv_OAL = pd.read_csv(f'{root_dir}.csv',
+    names = ['Manufacturer', 'System', 'Code', 'Occlusal', 'Apical', 'Length', 'Orientation','FMF path']
+)
+
 # create csv and row
 f = open('write.csv','w', newline='')
 wr = csv.writer(f)
@@ -24,6 +29,15 @@ def search_id(val):
 
     return system_id, manufact_id
 
+def search_OAL(file_code, system_name):
+    row_idx = int(csv_OAL.index[(csv_OAL["Code"] == file_code) & (csv_OAL["System"] == system_name)].tolist()[0])
+
+    occlusal = csv_OAL.iloc[row_idx][3]
+    apical = csv_OAL.iloc[row_idx][4]    
+    length = csv_OAL.iloc[row_idx][5]    
+
+    return occlusal, apical, length
+
 
 # export dir  
 for dir_path, dirs, files in os.walk(root_dir):
@@ -34,8 +48,9 @@ for dir_path, dirs, files in os.walk(root_dir):
         file_path = dir_path + '\\' + file_code
         
         system_id, manufact_id = search_id(system_name)
-
-        wr.writerow([manufact_id, '','','',file_path, '',system_id, file_code, 0.0])
+        occlusal, apical, length = search_OAL(file_code, system_name)
+        print(f'system name : {system_name}')
+        wr.writerow([manufact_id, occlusal, apical, length, file_path, '',system_id, file_code, 0.0])
 
         print(f'## file : {file_path} ')
 
